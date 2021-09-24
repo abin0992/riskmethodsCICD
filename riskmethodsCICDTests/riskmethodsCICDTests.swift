9a7c8f5edce9f0629d0f9fa472b5cc6fcce8674a
Abin Baby <abin.baby@riskmethods.net>
2021-09-24 14:01:45 +0530

    initial commit :wq

--- riskmethodsCICDTests/riskmethodsCICDTests.swift
+++ riskmethodsCICDTests/riskmethodsCICDTests.swift
@@ -18,16 +18,17 @@ class riskmethodsCICDTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test1() throws {
+        let a: Int = 13
+        let b: Int = 12
+        let c: Int = 25
+        XCTAssertEqual(c, (a+b))
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test2() throws {
+        let a = "This is testing"
+        XCTAssertNotNil(a)
     }
+
 
 }
